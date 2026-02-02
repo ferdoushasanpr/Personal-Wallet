af64e7d7541dc3784b946e20242c749b3d5d4129
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-02-02 23:26:57 +0600

    feat: Project Initialize

---
+++ android/app/src/main/kotlin/com/example/personalwallet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.personalwallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import "package:personalwallet/screens/home_screen.dart";
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallet Dark',
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
