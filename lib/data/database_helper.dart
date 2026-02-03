a79f9dfcb1843ab26fb9790e05ad2e2378a2752f
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-02-04 01:16:52 +0600

    feat: CreateDB Method Added to Database Helper

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  DatabaseHelper._init();
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'TEXT PRIMARY KEY';
+    const textType = 'TEXT NOT NULL';
+    const doubleType = 'REAL NOT NULL';
+    const intType = 'INTEGER NOT NULL';
+    const textNullable = 'TEXT';
+
+    await db.execute('''
+      CREATE TABLE accounts (
+        id $idType,
+        name $textType,
+        type $textType,
+        colorValue $intType,
+        initialBalance $doubleType,
+        currentBalance $doubleType
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE records (
+        id $idType,
+        accountId $textType,
+        targetAccountId $textNullable,
+        amount $doubleType,
+        type $intType,
+        category $textType,
+        date $textType
+      )
+    ''');
+
+    await db.execute('''
+  CREATE TABLE weekly_calculator (
+    id INTEGER PRIMARY KEY,
+    sun_earn REAL DEFAULT 0, sun_spend REAL DEFAULT 0,
+    mon_earn REAL DEFAULT 0, mon_spend REAL DEFAULT 0,
+    tue_earn REAL DEFAULT 0, tue_spend REAL DEFAULT 0,
+    wed_earn REAL DEFAULT 0, wed_spend REAL DEFAULT 0,
+    thu_earn REAL DEFAULT 0, thu_spend REAL DEFAULT 0,
+    fri_earn REAL DEFAULT 0, fri_spend REAL DEFAULT 0,
+    sat_earn REAL DEFAULT 0, sat_spend REAL DEFAULT 0
+  )
+''');
+  }
+}
